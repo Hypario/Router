9b9fdcf2f401f8cc88f0fd63472df43b41b6eb07
Hypario <fabienduterte@orange.fr>
2019-07-28 19:21:43 +0200

    removing params in request you must implement it

--- src/Router.php
+++ src/Router.php
@@ -100,12 +100,6 @@ class Router
             /** @var Route $route */
             foreach ($this->routes[$method] as $route) {
                 if ($route->match($url)) {
-                    if ($request instanceof ServerRequestInterface) {
-                        foreach ($route->getParams() as $key => $value) {
-                            $request = $request->withAttribute($key, $value);
-                        }
-                    }
-
                     return $route;
                 }
             }
